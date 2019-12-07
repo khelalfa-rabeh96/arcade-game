b6fdd465f59623c0e507dd9b14cc243cfb5a54e4
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-12-07 22:14:46 +0100

    Add the Enemy class arguments for Enemy's objects properties

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(posX, posY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    
+    this.x = posX;
+    this.y = posY;
+    this.speed = speed;
 };
 
 
