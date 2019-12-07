8830b7d5ba349594b0417d2c028f57dee634ff38
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-12-07 22:31:36 +0100

    Make the Enemies move

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    
+    this.x += this.speed *dt
 
    
 };
